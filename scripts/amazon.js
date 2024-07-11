26b195e679cba24fdab9327e227d86c92ece8d16
pushpaaxisrooms <pushpa@axisrooms.com>
2024-07-11 18:12:20 +0530

    used product js file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,33 +1,4 @@
-const products =[{
-    image : 'images/products/athletic-cotton-socks-6-pairs.jpg',
-    name : 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-    rating: {
-        stars : 4.5,
-        count:87
-    },
-    priceCents : 1090
-},
 
-{
-    image : 'images/products/intermediate-composite-basketball.jpg',
-    name : ' Intermediate Size Basketball',
-    rating: {
-        stars : 4,
-        count:127
-    },
-    priceCents : 2095
-},
-
-{
-    image : 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name : ' Adults Plain Cotton T-Shirt - 2 Pack',
-    rating: {
-        stars : 4.5,
-        count:56
-    },
-    priceCents : 799
-}
-];
 
 let productHTML = '';
 products.forEach((product)=>{
@@ -45,7 +16,7 @@ productHTML =  productHTML + `
 
 <div class="product-rating-container">
   <img class="product-rating-stars"k
-    src="${product.rating.stars * 100}">
+    src="${}">
   <div class="product-rating-count link-primary">
     ${product.rating.count}
   </div>
